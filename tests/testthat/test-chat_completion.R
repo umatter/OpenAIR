95230ec2e6e3b03c216a54aacfef4d625ac7c5ee
umatter <umatter@protonmail.com>
2023-03-06 15:10:03 +0100

    add tests

---
+++ tests/testthat/test-chat_completion.R
@@ -0,0 +1,12 @@
+test_that("Basic chat completion works", {
+msgs_df <- data.frame(role=c("system", 
+                             "user",
+                             "assistant",
+                             "user"),
+                      content=c("You are a helpful assistant", 
+                                "Who won the world series in 2020?",
+                                "The Los Angeles Dodgers won the World Series in 2020.",
+                                "Where was it played?"))
+chat_completion(messages = msgs_df)
+
+})
