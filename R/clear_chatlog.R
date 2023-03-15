9fd1e1381186a70b33f08e5ab9f397d72bf32c56
umatter <umatter@protonmail.com>
2023-03-15 23:26:30 +0100

    first commit of function to reset chatlog to system message

---
+++ R/clear_chatlog.R
@@ -0,0 +1,21 @@
+
+#' Clear a chat log
+#'
+#' This function clears a chat log, leaving only the initial (system) message.
+#'
+#' @param chatlog_id character string indicating the name of the chat log to clear.
+#'                   Default is ".__CURRENTCHAT__".
+#' @return This function does not return anything.
+#' @export
+#' @examples
+#' \dontrun{
+#' # Clear the current chat log
+#' clear_chatlog()
+#' }
+clear_chatlog <- function(chatlog_id = ".__CURRENTCHAT__") {
+  # get the current chatlog
+  current <- get_chatlog(chatlog_id)
+  # remove all but the initial (system) message
+  current@messages <- current@messages[1,]
+  assign(chatlog_id, current, envir = .GlobalEnv)
+}
