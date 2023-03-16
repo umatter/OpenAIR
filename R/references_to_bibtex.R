3c9b8d97fbe629a9536923cf9a400399fc140e05
umatter <umatter@protonmail.com>
2023-03-16 14:02:48 +0100

    fix minor bug, improve documentation

--- R/references_to_bibtex.R
+++ R/references_to_bibtex.R
@@ -1,11 +1,15 @@
 #' Convert references in plain text to BibTeX format
 #'
-#' This function takes a character string or file path to plain text references and 
-#' converts them to BibTeX format.
+#' This function takes a character string or a file path to plain text references and
+#' converts them into BibTeX format. The function reads the input text, processes it, and
+#' returns a character string containing the references in BibTeX format. If a file path is
+#' provided, the function also writes the BibTeX entries to a .bib file in the same
+#' directory.
 #'
-#' @param references A character string or file path to a file containing the plain text references to convert.
-#' @return A character string containing the references in BibTeX format.
-#' @author Ulrich Matter <umatter@protonmail.com>
+#' @param references A character string or a file path to a file containing the plain text references to convert.
+#' @return A character string containing the references in BibTeX format. If a file path is provided, the function also writes the BibTeX entries to a .bib file in the same directory and returns the file path of the newly created .bib file.
+#' @author Ulrich Matter umatter@protonmail.com
+#' @seealso \url{https://ctan.org/pkg/bibtex} for more information on BibTeX format
 #' @examples
 #' \dontrun{
 #' # Convert plain text references to BibTeX format
@@ -13,7 +17,7 @@
 #' Journal of Scientific Computing, 12, 45-67."
 #' references_to_bibtex(references)
 #' }
- #' @export
+#' @export
 
 
 references_to_bibtex <- function(references) {
@@ -40,18 +44,20 @@ references_to_bibtex <- function(references) {
     messages_content() %>% 
     clean_output() 
   
-  # test output
-  
-  
+
   # Return the processed references as BibTeX entries
-  if (references$file == "character string") {
+  filename <- unique(references$file)
+  if (filename == "character string") {
     message(output)
+    return(output)
+    
   } else {
-    output_file <- replace_file_extension(references$file)
+    output_file <- replace_file_extension(filename, new_extension = ".bib")
     writeLines(output, output_file)
+    return(output_file)
+    
   }
   
-  return(output)
 }
 
 
