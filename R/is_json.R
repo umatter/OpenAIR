c43d745f2cceaa03611067ae1ed6970ed5eab5a2
umatter <umatter@protonmail.com>
2023-03-21 08:54:13 +0100

    util functions

---
+++ R/is_json.R
@@ -0,0 +1,36 @@
+#' Check if the provided string is in valid JSON format.
+#'
+#' @param input_string A character string to be checked for JSON format.
+#'
+#' @return A logical value. If the input string is in valid JSON format, returns TRUE, otherwise returns FALSE.
+#'
+#' @author Ulrich Matter umatter@protonmail.com
+#'
+#' @examples
+#' is_json('{"name": "John", "age": 30}')
+#' # TRUE
+#'
+#' is_json('{"name": "John", age: 30}')
+#' # FALSE
+#'
+#' is_json('')
+#' # FALSE
+#' @export
+is_json <- function(input_string) {
+  if (!is.character(input_string)) {
+    warning("Input is not a character string.")
+    return(FALSE)
+  }
+  
+  if (nchar(input_string) == 0) {
+    warning("Empty string provided.")
+    return(FALSE)
+  }
+  
+  tryCatch({
+    jsonlite::fromJSON(input_string, simplifyVector = FALSE)
+    return(TRUE)
+  }, error = function(e) {
+    return(FALSE)
+  })
+}
