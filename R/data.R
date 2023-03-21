154092f60728f94bd610df17483031251d60f705
umatter <umatter@protonmail.com>
2023-03-21 22:19:54 +0100

    add input description

--- R/data.R
+++ R/data.R
@@ -65,3 +65,15 @@
 "json_to_csv_input"
 
 
+#' Input: message content template for tidyair()
+#'
+#' This message content serves as user input for the tidyair function.
+#'
+#' @format ## `tidyair`
+#' A data frame with 2 rows and 2 columns representing the messages object in API calls.
+#' @source Contributed by umatter
+#'
+"tidyair_input"
+
+
+
