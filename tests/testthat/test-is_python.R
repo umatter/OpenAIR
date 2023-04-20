8a6fdc04046c59ae5b76008f4e41ab41db0fba1b
JLDC <jonathan.chassot@unisg.ch>
2023-04-20 09:10:10 +0200

    Check for python before is_python test

--- tests/testthat/test-is_python.R
+++ tests/testthat/test-is_python.R
@@ -1,20 +1,23 @@
+if(all(nchar(Sys.which("python", "python3"))) == 0) {
+  skip("Python not installed, skipping test.")
+} else {
+  # Test that the is_python function recognizes valid Python code
+  test_that("is_python recognizes valid Python code", {
+    expect_true(is_python("print('Hello, World!')"))
+    expect_true(is_python("x = 5"))
+    code <-
+  "
+  x = 5
+  if x > 0:
+      print('x is positive')
+  "
+    expect_true(is_python(code))
+  })
 
-# Test that the is_python function recognizes valid Python code
-test_that("is_python recognizes valid Python code", {
-  expect_true(is_python("print('Hello, World!')"))
-  expect_true(is_python("x = 5"))
-  code <- 
-"
-x = 5
-if x > 0:
-    print('x is positive')
-"
-  expect_true(is_python(code))
-})
-
-# Test that the is_python function recognizes invalid Python code
-test_that("is_python recognizes invalid Python code", {
-  expect_false(is_python("prit('Hello, World!')"))
-  expect_false(is_python("x == 5"))
-  expect_false(is_python("if x > 0\n    print('x is positive')"))
-})
+  # Test that the is_python function recognizes invalid Python code
+  test_that("is_python recognizes invalid Python code", {
+    expect_false(is_python("prit('Hello, World!')"))
+    expect_false(is_python("x == 5"))
+    expect_false(is_python("if x > 0\n    print('x is positive')"))
+  })
+}
